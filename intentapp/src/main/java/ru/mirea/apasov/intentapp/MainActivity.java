fb38c594c1aa1779553d2939e90903d6589eb9bb
Vasily Apasov <102738486+codemdvd@users.noreply.github.com>
2022-04-25 00:09:56 +0300

    Initial commit

---
+++ intentapp/src/main/java/ru/mirea/apasov/intentapp/MainActivity.java
@@ -0,0 +1,34 @@
+package ru.mirea.apasov.intentapp;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void onClickTime(View view) {
+        long dateInMillis = System.currentTimeMillis();
+        String format = "yyyy-MM-dd HH:mm:ss";
+        final SimpleDateFormat sdf = new SimpleDateFormat(format);
+        String dateString = sdf.format(new Date(dateInMillis));
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra("time",dateString );
+        startActivity(intent);
+    }
+
+}
---
+++ intentapp/src/main/java/ru/mirea/apasov/intentapp/SecondActivity.java
@@ -0,0 +1,32 @@
+package ru.mirea.apasov.intentapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import ru.mirea.apasov.intentapp.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String string = intent.getStringExtra("time");
+        TextView text = findViewById(R.id.textView);
+        text.setText(string);
+    }
+}
